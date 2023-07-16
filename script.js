7e91bb3b54bf55ca6c26d8a96578e1964024f781
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-16 22:15:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+let givenString = prompt("Enter a string");
+
+function firstNonRepeatingCharacter(string) {
+	for(let i = 0;i<string.length;i++){
+		if(string.lastIndexOf(string[i])==i){
+			return string[i];
+		}
+	}	
+}
+alert(firstNonRepeatingCharacter(givenString));
